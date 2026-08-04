0bd2b996a5534afa570a32f4cff8fa4b211cdbe0
moritzmolch <moritz.molch@kit.edu>
2026-08-04 19:52:23 +0200

    Format code

--- src/xyh/config/campaigns/__init__.py
+++ src/xyh/config/campaigns/__init__.py
@@ -5,14 +5,12 @@ simulation sample metadata.
 
 from order import Campaign, UniqueObjectIndex
 
-
 from .cpn_2022_post_ee_nano_v12 import cpn_2022_post_ee_nano_v12
 from .cpn_2022_pre_ee_nano_v12 import cpn_2022_pre_ee_nano_v12
 from .cpn_2023_post_bpix_nano_v12 import cpn_2023_post_bpix_nano_v12
 from .cpn_2023_pre_bpix_nano_v12 import cpn_2023_pre_bpix_nano_v12
 from .cpn_2024_nano_v15 import cpn_2024_nano_v15
 from .cpn_2025_nano_v15 import cpn_2025_nano_v15
-
 
 # Container for all campaigns
 campaigns = UniqueObjectIndex(
