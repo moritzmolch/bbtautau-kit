f53bca16096cc672952c506bfba13fd806e8b7a8
moritzmolch <moritz.molch@kit.edu>
2026-09-11 09:16:15 +0200

    Refactor processes setup

--- src/xyh/config/processes/__init__.py
+++ src/xyh/config/processes/__init__.py
@@ -6,23 +6,42 @@ from itertools import chain
 
 from order import Process, UniqueObjectIndex
 
-# Import process indices from submodules
-from .data import processes as data_processes
-from .ewk import processes as ewk_processes
-from .higgs import processes as higgs_processes
-from .jetfakes import processes as jetfakes_processes
-from .top import processes as top_processes
-from .xyh import processes as xyh_processes
+from ._data import get_data_processes
+from ._ewk import get_vv_processes, get_w_processes, get_z_processes
+from ._higgs import (
+    get_hh_2b2tau_processes,
+    get_higgs_2b_processes,
+    get_higgs_2tau_processes,
+    get_tth_processes,
+)
+from ._jetfakes import get_jetfakes_processes
+from ._top import get_single_t_processes, get_tt_processes
+from ._xyh import get_xyh_processes
+
+# Only expose the processes index
+__all__ = ["processes"]
 
 # Merge all processes into a single index
 processes = UniqueObjectIndex(
     Process,
     chain(
-        data_processes.values(),
-        ewk_processes.values(),
-        higgs_processes.values(),
-        jetfakes_processes.values(),
-        top_processes.values(),
-        xyh_processes.values(),
+        # Data
+        get_data_processes(),
+        # Electroweak processes
+        get_z_processes(),
+        get_w_processes(),
+        get_vv_processes(),
+        # Higgs processes
+        get_higgs_2tau_processes(),
+        get_higgs_2b_processes(),
+        get_tth_processes(),
+        get_hh_2b2tau_processes(),
+        # Jet -> tau_h misidentification
+        get_jetfakes_processes(),
+        # Top quark processes
+        get_tt_processes(),
+        get_single_t_processes(),
+        # X -> HY signal processes
+        get_xyh_processes(),
     ),
 )
---
+++ src/xyh/config/processes/_xyh.py
@@ -0,0 +1,35 @@
+from itertools import product
+
+from order import Process
+
+from xyh.config.profiles import get_profile
+
+from ._util import add_process
+
+
+def get_xyh_processes() -> list[Process]:
+    """
+    X -> HY -> b b tau tau signal processes
+    """
+
+    # List of processes
+    processes = []
+
+    for (y_decay_mode, h_decay_mode), (m_x, m_y) in product(
+        get_profile().decay_modes, get_profile().xy_masses
+    ):
+        # Add the process for this specific hypothesis to the X -> HY parent
+        # processs
+        add_process(
+            name=f"xyh_{y_decay_mode}_{h_decay_mode}_mx{m_x}_my{m_y}",
+            id="+",
+            is_data=False,
+            tags=xyh.tags,
+            aux={
+                "is_signal": True,
+                "y_decay_mode": y_decay_mode,
+                "h_decay_mode": h_decay_mode,
+                "m_x": m_x,
+                "m_y": m_y,
+            },
+        )
